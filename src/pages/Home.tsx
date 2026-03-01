21902056bd1fc22347d56eafb722e0363f3b6d7a
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2026-03-01 21:26:47 +0100

    lint

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,47 +1,46 @@
-import { useEffect, useState } from "react";
-import { Caroussel, type Slide } from "../components/Caroussel/Caroussel";
-import { Footer } from "../components/Footer/Footer";
-import { Header } from "../components/Header/Header";
+import { useEffect, useState } from 'react'
+
+import { Caroussel, type Slide } from '../components/Caroussel/Caroussel'
+import { Footer } from '../components/Footer/Footer'
+import { Header } from '../components/Header/Header'
 
 export function Home() {
-    const [slides, setSlides] = useState<Slide[]>([]);
+  const [slides, setSlides] = useState<Slide[]>([])
 
-     useEffect(() => {
-  const fetchedSlides: Slide[] = [
-  {
-    imgSrc: "https://picsum.photos/800/600?random=1",
-    name: "Paisagem",
-    destination: "/portfolio",
-  },
-  {
-    imgSrc: "https://picsum.photos/800/600?random=2",
-    name: "Cidade",
-    destination: "/about",
-  },
-  {
-    imgSrc: "https://picsum.photos/800/600?random=3",
-    name: "Natureza",
-    destination: "/services",
-  },
-  {
-    imgSrc: "https://picsum.photos/800/600?random=4",
-    name: "Arte",
-    destination: "/contact",
-  },
-];
+  useEffect(() => {
+    const fetchedSlides: Slide[] = [
+      {
+        imgSrc: 'https://picsum.photos/800/600?random=1',
+        name: 'Paisagem',
+        destination: '/portfolio',
+      },
+      {
+        imgSrc: 'https://picsum.photos/800/600?random=2',
+        name: 'Cidade',
+        destination: '/about',
+      },
+      {
+        imgSrc: 'https://picsum.photos/800/600?random=3',
+        name: 'Natureza',
+        destination: '/services',
+      },
+      {
+        imgSrc: 'https://picsum.photos/800/600?random=4',
+        name: 'Arte',
+        destination: '/contact',
+      },
+    ]
 
-    setSlides(fetchedSlides);
-  }, []);
+    setSlides(fetchedSlides)
+  }, [])
 
-   return (
+  return (
     <div className="flex min-h-screen flex-col">
       <Header />
 
       <main className="flex flex-1 flex-col items-center justify-center gap-6">
-        <p className="text-neutral-600 text-lg">
-          Bem-vinda ao Verno Portfolio.
-        </p>
-        
+        <p className="text-neutral-600 text-lg">Bem-vinda ao Verno Portfolio.</p>
+
         {slides.length > 0 ? (
           <div className="w-1/2">
             <Caroussel slides={slides} />
@@ -49,10 +48,9 @@ export function Home() {
         ) : (
           <p>Carregando slides...</p>
         )}
-        
       </main>
 
       <Footer />
     </div>
-  );
+  )
 }
