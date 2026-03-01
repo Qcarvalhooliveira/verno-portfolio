14d6816ffb6c5c8144a2c77ce1a92a6ef4e72f30
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-03-01 19:10:36 +0100

    chre[pages/home]: display caroussel in home page
    (just to visualize)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,58 @@
+import { useEffect, useState } from "react";
+import { Caroussel, type Slide } from "../components/Caroussel/Caroussel";
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
 
 export function Home() {
-  return (
+    const [slides, setSlides] = useState<Slide[]>([]);
+
+     useEffect(() => {
+  const fetchedSlides: Slide[] = [
+  {
+    imgSrc: "https://picsum.photos/800/600?random=1",
+    name: "Paisagem",
+    destination: "/portfolio",
+  },
+  {
+    imgSrc: "https://picsum.photos/800/600?random=2",
+    name: "Cidade",
+    destination: "/about",
+  },
+  {
+    imgSrc: "https://picsum.photos/800/600?random=3",
+    name: "Natureza",
+    destination: "/services",
+  },
+  {
+    imgSrc: "https://picsum.photos/800/600?random=4",
+    name: "Arte",
+    destination: "/contact",
+  },
+];
+
+    setSlides(fetchedSlides);
+  }, []);
+
+   return (
     <div className="flex min-h-screen flex-col">
       <Header />
 
-      <main className="flex flex-1 items-center justify-center">
-        <p className="text-neutral-600">
+      <main className="flex flex-1 flex-col items-center justify-center gap-6">
+        <p className="text-neutral-600 text-lg">
           Bem-vinda ao Verno Portfolio.
         </p>
+        
+        {slides.length > 0 ? (
+          <div className="w-1/2">
+            <Caroussel slides={slides} />
+          </div>
+        ) : (
+          <p>Carregando slides...</p>
+        )}
+        
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
