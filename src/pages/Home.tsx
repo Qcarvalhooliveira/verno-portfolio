2d8aa1ad79603b3a0bc6ce7f689516bd67bb126c
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2026-03-04 20:19:10 +0100

    feat[pages/Home]: create sections for services in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { Caroussel, type Slide } from '../components/Caroussel/Caroussel'
 import { Footer } from '../components/Footer/Footer'
 import { Header } from '../components/Header/Header'
+import { ServiceStamp } from '../components/ServiceStamp/ServiceStamp'
 
 export function Home() {
   const [slides, setSlides] = useState<Slide[]>([])
@@ -39,7 +40,7 @@ export function Home() {
       <Header />
 
       <main className="flex flex-1 flex-col items-center justify-center gap-6">
-        <p className="text-neutral-600 text-lg">Bem-vinda ao Verno Portfolio.</p>
+        <p className="text-neutral-600 text-lg">Bem-vindos ao Verno Portfolio.</p>
 
         {slides.length > 0 ? (
           <div className="w-1/2">
@@ -48,6 +49,39 @@ export function Home() {
         ) : (
           <p>Carregando slides...</p>
         )}
+        <section className="flex flex-col ml-10 w-full items-center">
+          <a href="/servicos">
+            <h2 className="text-3xl tracking-widest uppercase bold font-serif bg-slate-950 text-amber-50 w-fit">
+              Nossos serviços
+            </h2>
+          </a>
+          <div className="flex gap-30">
+            <div className="flex flex-col justify-between gap-7 py-5 ">
+              <ServiceStamp name="Design Editorial" href="/servicos#edicao" delay={0} />
+
+              <ServiceStamp name="Planejamento Gráfico" href="/servicos#revisao" delay={200} />
+
+              <ServiceStamp
+                name="Diagramação Profissional"
+                href="/servicos#diagramação"
+                delay={400}
+              />
+            </div>
+            <div className="flex flex-col justify-between gap-7 py-5">
+              <ServiceStamp name="Edição de Arte" href="/servicos#edição" delay={0} />
+
+              <ServiceStamp name="Produção gráfica" href="/servicos#gráfica" delay={200} />
+
+              <ServiceStamp
+                name="Produção de Publicações"
+                href="/servicos#publicações"
+                delay={400}
+              />
+
+              <ServiceStamp name="Desenvolvimento de E-books" href="/servicos#ebooks" delay={600} />
+            </div>
+          </div>
+        </section>
       </main>
 
       <Footer />
